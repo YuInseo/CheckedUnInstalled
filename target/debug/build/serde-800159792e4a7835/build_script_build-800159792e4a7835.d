d9317cef2f624f2d06446a41422b33f5f737b378
YuInseo <inseo0121@gmail.com>
2024-04-16 22:00:44 +0900

    feat: removed unnecessary files

--- target/debug/build/serde-800159792e4a7835/build_script_build-800159792e4a7835.d
+++
@@ -1,5 +0,0 @@
-D:\Project\CLI\CheckUninstalled\target\debug\build\serde-800159792e4a7835\build_script_build-800159792e4a7835.exe: C:\Users\first\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde-1.0.197\build.rs
-
-D:\Project\CLI\CheckUninstalled\target\debug\build\serde-800159792e4a7835\build_script_build-800159792e4a7835.d: C:\Users\first\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde-1.0.197\build.rs
-
-C:\Users\first\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde-1.0.197\build.rs:
--- target/debug/build/winapi-c020c43fb8dd2142/build_script_build-c020c43fb8dd2142.d
+++
@@ -1,5 +0,0 @@
-D:\Project\CLI\CheckUninstalled\target\debug\build\winapi-c020c43fb8dd2142\build_script_build-c020c43fb8dd2142.exe: C:\Users\first\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs
-
-D:\Project\CLI\CheckUninstalled\target\debug\build\winapi-c020c43fb8dd2142\build_script_build-c020c43fb8dd2142.d: C:\Users\first\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs
-
-C:\Users\first\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs:
